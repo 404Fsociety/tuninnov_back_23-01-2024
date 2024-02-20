44017ebf5e3dfcbc63449ca53d1e7b5092e8c37f
Nizar May <133237153+404Fsociety@users.noreply.github.com>
2024-02-20 17:50:56 +0100

    Update vcf.js

--- controller/vcf.js
+++ controller/vcf.js
@@ -84,8 +84,8 @@ const generateVCFContent = (userData) => {
     vcfString += "BEGIN:VCARD\r\n";
     vcfString += "VERSION:3.0\r\n";
 
-    vcfString +=`N:${(userData.lastname || '')};${(userData.firstname || '')};${(userData.prefix || '')};${(userData.suffix || '')}\r\n`;
-    vcfString += `FN:${userData.lastname};${userData.prefix || ''};${userData.firstname} ${userData.suffix || ''}\r\n`;
+    vcfString += `N:${(userData.lastname || '')};${(userData.firstname || '')}\r\n`;
+    vcfString += `FN:${userData.lastname} ${userData.firstname}\r\n`;
     vcfString += `NICKNAME:${userData.nickname || ''}\r\n`;
     const base64Image = userData.photo.replace(/^data:image\/(png|jpeg);base64,/, '');
     vcfString += `PHOTO:${base64Image}\r\n`;
